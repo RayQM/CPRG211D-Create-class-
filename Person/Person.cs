4ba8d1886081477f0bbd870255e6b6fabecdf8de
Qiaomu Lei <115581793+RayQM@users.noreply.github.com>
2023-01-24 19:28:39 -0700

    1
    1

---
+++ Person/Person.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml;
+
+namespace Person
+{
+    internal class Person
+    {
+        private int id;
+        private string fistName;
+        private string lastName;
+        private string favoriteColor;
+        private int age;
+        private bool isWorking;
+
+        public int Id { get => this.id; set => this.id = value; }
+        public string FistName { get => this.fistName; set => this.fistName = value; }
+        public string LastName { get => this.lastName; set => this.lastName = value; }
+        public string FavoriteColor { get => this.favoriteColor; set => this.favoriteColor = value; }
+        public int Age { get => this.age; set => this.age = value; }
+        public bool IsWorking { get => this.isWorking; set => this.isWorking = value; }
+
+
+        public Person(int id, string fistName, string lastName,string favoriteColor, int age, bool isWorking) 
+        {   this.id = id;
+            this.fistName = fistName; 
+            this.lastName = lastName;
+            this.favoriteColor = favoriteColor;
+            this.age = age;
+            this.isWorking = isWorking;
+        }
+
+   
+
+        public override string ToString()
+        {
+            string formatted=" ";
+
+            formatted += "PersonID:"+ id + "\n";
+            formatted += "Frist name:" + FistName +"\n";
+            formatted += "Last name: " + LastName + "\n";
+            formatted += "Favorite Colour: " + FavoriteColor + "\n";
+            formatted += "age: " + age + "\n";
+            formatted += "Is working: " + isWorking + "\n";
+            return formatted;
+        }
+       
+        public void DisplayPersonInfo()
+        {
+            Console.WriteLine( this.id+":" + this.fistName + this.lastName + " favorite color is " + this.favoriteColor);
+        }
+
+        public void ChangeFavoriteColor()
+        {
+            this.favoriteColor = "White";
+            Console.WriteLine(this.id + ":" + this.fistName + this.lastName + " favorite color is " + this.favoriteColor);
+        }
+       
+        public void GetAgeInTenYears()
+        {
+            this.age += 10;
+            Console.WriteLine(this.id + ":" + this.fistName + this.lastName + " age in 10 years is :" + this.age);
+        }
+    }
+
+}
---
+++ Person/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Person person1 = new Person(1,"Ian","Brooks","Red",30,true);
+            Person person2 = new Person(2,"Gina","James","Green",20,false);
+            Person person3 = new Person(3,"Mike","Briscoe","Blue",45,true);
+            Person person4 = new Person(4,"Mary","Beals","Yellow",28,true);
+
+
+
+            person2.DisplayPersonInfo();
+
+            person1.ChangeFavoriteColor();
+
+            person4.GetAgeInTenYears();
+            
+            Console.WriteLine(person1.ToString());
+            Console.WriteLine(person2.ToString());
+            Console.WriteLine(person3.ToString());
+            Console.WriteLine(person4.ToString());
+
+        }
+    }
+}
